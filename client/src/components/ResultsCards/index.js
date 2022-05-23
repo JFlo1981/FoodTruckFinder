f5c60c432350a175363018f5fe0be96180621761
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-23 18:34:06 -0500

    Adds example result cards to Search Page

--- client/src/components/ResultsCards/index.js
+++ client/src/components/ResultsCards/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Badge, Image, Flex } from "@chakra-ui/react";
+import { Box, Image, Flex, Button, Link } from "@chakra-ui/react";
 
 export default function resultCards() {
   const property = {
@@ -7,7 +7,7 @@ export default function resultCards() {
       "https://images.unsplash.com/photo-1620589125156-fd5028c5e05b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1787",
     imageAlt: "Rear view of modern home with pool",
     title: "Fish & Chips",
-    formattedPrice: "9am-5pm",
+    hours: "9am-5pm",
     reviewCount: 34,
     rating: 4,
   };
@@ -21,42 +21,35 @@ export default function resultCards() {
           roundedTop="lg"
         />
 
-        <Box p="6">
-          <Box d="flex" alignItems="baseline">
-            <Badge rounded="full" px="2" colorScheme="teal">
-              Seafood
-            </Badge>
-            <Badge rounded="full" px="2" colorScheme="teal" ml="2">
-              British
-            </Badge>
-          </Box>
-
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            isTruncated
-          >
-            {property.title}
-          </Box>
-
-          <Box>{property.formattedPrice}</Box>
-
-          <Box d="flex" mt="2" alignItems="center">
-            {/* {Array(5)
-              .fill("")
-              .map((_, i) => (
-                <StarIcon
-                  key={i}
-                  color={i < property.rating ? "teal.500" : "gray.300"}
-                />
-              ))} */}
-            <Box as="span" ml="2" color="gray.600" fontSize="sm">
-              {property.reviewCount} reviews
+        <Flex alignItems="center" justifyContent="space-around">
+          <Box p="6">
+            <Box
+              mt="1"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              isTruncated
+            >
+              {property.title}
             </Box>
+
+            <Box>{property.hours}</Box>
           </Box>
-        </Box>
+
+          <Button
+            rounded={"full"}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bg={"green.400"}
+            _hover={{
+              bg: "green.300",
+            }}
+            marginTop={"5"}
+          >
+            <Link href={"#"}>View Truck</Link>
+          </Button>
+        </Flex>
       </Box>
     </Flex>
   );
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,54 @@
 import React from "react";
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Icon,
+  useColorModeValue,
+  createIcon,
+} from "@chakra-ui/react";
+import ResultCards from "../components/ResultsCards";
 
 function Search() {
   return (
     <div>
-      <h1>Search coming soon</h1>
+      <Container maxW={"3xl"} height="70vh">
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Map Goes <br />
+            <Text as={"span"} color={"green.400"}>
+              here
+            </Text>
+          </Heading>
+        </Stack>
+      </Container>
+
+      <Stack
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"lg"}
+        borderTop={"5px solid #c9c9c9"}
+        p={8}
+        rounded={"xl"}
+        align={"center"}
+        pos={"relative"}
+      >
+        <Heading>Results for "City"</Heading>
+        <ResultCards />
+        <ResultCards />
+        <ResultCards />
+      </Stack>
     </div>
   );
 }
