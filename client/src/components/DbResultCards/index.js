40111a0249dc291dcefa580dd188201f07011951
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-31 14:05:07 -0500

    Adds both database and Austin data to populate in search page

---
+++ client/src/components/DbResultCards/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, Image, Button, Link } from "@chakra-ui/react";
+
+const DbResultCards = ({ trucks }) => {
+  return (
+    <>
+      {trucks &&
+        trucks.map((truck) => (
+          <Box
+            key={truck.truckName}
+            maxW="sm"
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+          >
+            <Image
+              src={truck.image}
+              alt={"food truck"}
+              roundedTop="lg"
+              height={"200px"}
+              objectFit={"cover"}
+              width={"100%"}
+            />
+
+            <Box textAlign={"center"}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {truck.truckName}
+              </Box>
+
+              <Box>{truck.hours}</Box>
+
+              <Button
+                rounded={"full"}
+                fontSize={"sm"}
+                fontWeight={600}
+                color={"white"}
+                bg={"green.400"}
+                _hover={{
+                  bg: "green.300",
+                }}
+                marginTop={"2"}
+                marginBottom={"4"}
+                width={"80%"}
+              >
+                <Link href={"/truck"}>View Truck</Link>
+              </Button>
+            </Box>
+          </Box>
+        ))}
+    </>
+  );
+};
+
+export default DbResultCards;
--- client/src/components/ResultsCards/index.js
+++ client/src/components/ResultsCards/index.js
@@ -1,71 +1,64 @@
 import React from "react";
-import { Box, Image, Flex, Button, Link, SimpleGrid } from "@chakra-ui/react";
+import { Box, Image, Button, Link } from "@chakra-ui/react";
 import austinData from "../data";
 
-const resultCards = ({ trucks }) => {
+const resultCards = () => {
   // console.log(austinData);
   return (
-    <Flex p={50} w="full" alignItems="center" justifyContent="center">
-      <SimpleGrid
-        columns={{ base: 1, md: 2, xl: 4 }}
-        spacing={"20"}
-        mt={5}
-        mx={"auto"}
-      >
-        {austinData &&
-          austinData.map(
-            (truck, index) =>
-              index < 5 && (
-                <Box
-                  key={truck.id}
-                  maxW="sm"
-                  borderWidth="1px"
-                  rounded="lg"
-                  shadow="lg"
-                >
-                  <Image
-                    src={truck.image_url}
-                    alt={"food truck"}
-                    roundedTop="lg"
-                    height={"200px"}
-                    objectFit={"cover"}
-                    width={"100%"}
-                  />
+    <>
+      {austinData &&
+        austinData.map(
+          (truck, index) =>
+            index < 5 && (
+              <Box
+                key={truck.id}
+                maxW="sm"
+                borderWidth="1px"
+                rounded="lg"
+                shadow="lg"
+              >
+                <Image
+                  src={truck.image_url}
+                  alt={"food truck"}
+                  roundedTop="lg"
+                  height={"200px"}
+                  objectFit={"cover"}
+                  width={"100%"}
+                />
 
-                  <Box textAlign={"center"}>
-                    <Box
-                      mt="1"
-                      fontWeight="semibold"
-                      as="h4"
-                      lineHeight="tight"
-                      isTruncated
-                    >
-                      {truck.name}
-                    </Box>
-
-                    <Box>Rating: {truck.rating} stars</Box>
-
-                    <Button
-                      rounded={"full"}
-                      fontSize={"sm"}
-                      fontWeight={600}
-                      color={"white"}
-                      bg={"green.400"}
-                      _hover={{
-                        bg: "green.300",
-                      }}
-                      marginTop={"2"}
-                      marginBottom={"4"}
-                      width={"80%"}
-                    >
-                      <Link href={`/truck/${truck.id}`}>View Truck</Link>
-                    </Button>
+                <Box textAlign={"center"}>
+                  <Box
+                    mt="1"
+                    fontWeight="semibold"
+                    as="h4"
+                    lineHeight="tight"
+                    isTruncated
+                  >
+                    {truck.name}
                   </Box>
+
+                  <Box>Rating: {truck.rating} stars</Box>
+
+                  <Button
+                    rounded={"full"}
+                    fontSize={"sm"}
+                    fontWeight={600}
+                    color={"white"}
+                    bg={"green.400"}
+                    _hover={{
+                      bg: "green.300",
+                    }}
+                    marginTop={"2"}
+                    marginBottom={"4"}
+                    width={"80%"}
+                  >
+                    <Link href={`/truck/${truck.id}`}>View Truck</Link>
+                  </Button>
                 </Box>
-              )
-          )}
-      </SimpleGrid>
-    </Flex>
+              </Box>
+            )
+        )}
+    </>
   );
 };
 
